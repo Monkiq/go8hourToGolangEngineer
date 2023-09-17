8a8490dc833e59abf2f92c92bf5ed8dc440d8149
monkiqchen <monkiqchen@tencent.com>
2023-09-17 22:04:51 +0800

    版本9.1 菜单显示

--- client.go
+++ client.go
@@ -11,12 +11,14 @@ type Client struct {
 	ServerPort int
 	Name       string
 	conn       net.Conn
+	flag       int
 }
 
 func NewClient(serverIp string, serverPort int) (*Client, error) {
 	client := &Client{
 		ServerIp:   serverIp,
 		ServerPort: serverPort,
+		flag:       999,
 	}
 
 	conn, err := net.Dial("tcp", fmt.Sprintf("%s:%d", serverIp, serverPort))
@@ -28,6 +30,41 @@ func NewClient(serverIp string, serverPort int) (*Client, error) {
 	client.conn = conn
 
 	return client, nil
+}
+
+func (c *Client) menu() bool {
+	fmt.Println("菜单选择")
+	fmt.Println("1. 群聊模式")
+	fmt.Println("2. 私聊模式")
+	fmt.Println("3. 更新用户名")
+	fmt.Println("0. 退出")
+
+	var flag int
+	fmt.Scanln(&flag)
+	if flag >= 0 && flag < 4 {
+		c.flag = flag
+		return true
+	} else {
+		fmt.Println("请输入合法的命令")
+		return false
+	}
+}
+
+func (c *Client) Run() {
+	for c.flag != 0 {
+		for c.menu() != true {
+		}
+		switch c.flag {
+		case 1:
+			fmt.Println("你选择群聊模式")
+		case 2:
+			fmt.Println("你选择的是私聊模式")
+		case 3:
+			fmt.Println("你选择更新用户名模式")
+		}
+	}
+	fmt.Println("程序即将退出")
+	c.conn.Close()
 }
 
 var serverIp string
@@ -45,7 +82,7 @@ func main() {
 	if err != nil {
 		fmt.Print("连接服务器失败,请检查连接参数")
 		return
-	} else {
-		fmt.Println(c.ServerIp + " 连接成功\n")
 	}
+	fmt.Println(c.ServerIp + " 连接成功\n")
+	c.Run()
 }
